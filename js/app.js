ed6c23257d3a36ef9e6c5cd2115e7ef24060fa6d
SQLAdmin <abuyeahia24@gmail.com>
2023-02-26 13:32:41 +0600

    show all added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-const loadPhone =async(searchText) =>{
+const loadPhone =async(searchText, dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
      const res = await fetch(url);
      const data = await res.json();
-     displayPhones(data.data)
+     displayPhones(data.data, dataLimit)
 }
 
-const displayPhones = phones =>{
+const displayPhones = (phones, dataLimit) =>{
     // console.log(phones)
     const phoneContainer = document.getElementById('phone-container')
     phoneContainer.textContent = '';
     
-    // display 20 phone only
-    phones = phones.slice(0, 3);
+    // display 10 phone only
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+      phones = phones.slice(0, 10);
+     showAll.classList.remove('d-none')
+    }else{
+      showAll.classList.add('d-none')
+    }
 
     // display no phone found
     const noPhone = document.getElementById('no-found-message');
@@ -41,13 +47,16 @@ const displayPhones = phones =>{
     toggleSpinner(false)
 
 }
-
-document.getElementById('btn-search').addEventListener('click', function(){
-  // start loader
+const processSearch = (dataLimit) =>{
   toggleSpinner(true)
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
-  loadPhone(searchText);
+  loadPhone(searchText, dataLimit);
+}
+// handler search button click
+document.getElementById('btn-search').addEventListener('click', function(){
+  // start loader
+processSearch(10)
 })
 const toggleSpinner = isLoading =>{
   const loaderSection = document.getElementById('loader')
@@ -58,4 +67,8 @@ const toggleSpinner = isLoading =>{
   }
 }
 
+// not best solution for load all data in api
+document.getElementById('btn-show-all').addEventListener('click', function(){
+  processSearch()
+})
 // loadPhone();
