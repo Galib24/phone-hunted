9b7b16aca2eef86a658cda86de7fe56fe7b61180
SQLAdmin <abuyeahia24@gmail.com>
2023-02-28 01:02:54 +0600

    type of js

---
+++ js/types.js
@@ -0,0 +1,11 @@
+//js is a  dynamic typed language 
+
+// primitive
+const a = 5;
+const b = 'abu kabul babu';
+const d  = true;
+
+// non primitive
+const ages = [34,45,64];
+const student = {id:23, class: 7};
+// console.log(typeof a, typeof b, typeof ages, typeof student);
