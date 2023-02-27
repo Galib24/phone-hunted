3ff2d04bf3f7db53510974f0b3735e89bd6a228f
SQLAdmin <abuyeahia24@gmail.com>
2023-02-28 02:40:11 +0600

    true false added js

---
+++ js/true-false.js
@@ -0,0 +1,18 @@
+// 1) true or false
+const x = false;
+if(x){
+    console.log('value of x is truth')
+}else{
+    console.log('value of x is false')
+}
+// 2)any number positive or negative will be truth except 0 will false
+const x2 = -6;
+if(x){
+    console.log('value of x is truth')
+}else{
+    console.log('value of x is false')
+}
+// 3) '' empty string will false(-) but ' ' this will be true (+) in middle gap
+const man = '';
+// 4) '0' will be true 'false' also will true
+// undefine will be false {} empty object will truth [] also true
