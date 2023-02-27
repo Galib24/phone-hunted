91bfa8b3047cd62cb2dcc888f380660a59d06ed0
SQLAdmin <abuyeahia24@gmail.com>
2023-02-28 03:05:10 +0600

    equal type added in js

---
+++ js/equal.js
@@ -0,0 +1,8 @@
+const first = 2;
+const second = 2;
+if(first == second){
+    console.log('value are equal')
+}else{
+    console.log('value not equal')
+}
+// equal comparison doesn't work for non-primitive
