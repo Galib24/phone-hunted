c7f538afdc13f7edcdad8a5342e9d283bb39131b
SQLAdmin <abuyeahia24@gmail.com>
2023-02-27 17:18:06 +0600

    added

--- js/compair.js
+++ js/compair.js
@@ -37,7 +37,15 @@ const isSame = comPairObject(first1, second2)
 var examPass = true;
 var markShit = 40;
 if(examPass == true || markShit >50){
-    console.log('pass')
+    // console.log('pass')
 }else{
-    console.log('fail')
+    // console.log('fail')
 }
+// const obj ={a:1, b:7, c:3, length:2};
+// console.log(Object.keys(obj).length);
+
+// const obj1= {module: 35, video:2}; 
+// const obj2= {module: 35, video:2};
+// console.log(obj1 === obj2);
+
+const getGirlFriend= (name = "chokina")=>"name"; console.log(getGirlFriend()); 
